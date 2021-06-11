28435432057614130acd06392ee98099372ce7e0
kelnaazshiekho97 <julianath7896@gmail.com>
2021-06-12 02:18:59 +0300

    add user email and name to navbar

--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -3,9 +3,11 @@
  <nav class="admin-header navbar navbar-default col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <!-- logo -->
     <div class="text-left navbar-brand-wrapper">
-        <a class="navbar-brand brand-logo" href="index.html"><img src="assets/images/logo-dark.png" alt=""></a>
-        <a class="navbar-brand brand-logo-mini" href="index.html"><img src="assets/images/logo-icon-dark.png"
+        <a class="navbar-brand brand-logo" href="{{ url('/dashboard') }}"><img src="{{ URL::asset('assets/images/logo-dark.png') }}" alt=""></a>
+        <a class="navbar-brand brand-logo-mini" href="{{ url('/dashboard') }}"><img src="{{ URL::asset('assets/images/logo-icon-dark.png') }}"
                 alt=""></a>
+
+
     </div>
     <!-- Top bar left -->
     <ul class="nav navbar-nav mr-auto">
@@ -100,14 +102,14 @@
         <li class="nav-item dropdown mr-30">
             <a class="nav-link nav-pill user-avatar" data-toggle="dropdown" href="#" role="button"
                 aria-haspopup="true" aria-expanded="false">
-                <img src="assets/images/profile-avatar.jpg" alt="avatar">
+                <img src="{{ URL::asset('assets/images/user_icon.png') }}" alt="avatar">
             </a>
             <div class="dropdown-menu dropdown-menu-right">
                 <div class="dropdown-header">
                     <div class="media">
                         <div class="media-body">
-                            <h5 class="mt-0 mb-0">Michael Bean</h5>
-                            <span>michael-bean@mail.com</span>
+                            <h5 class="mt-0 mb-0">{{ Auth::user()->name }}</h5>
+                            <span>{{ Auth::user()->email }}</span>
                         </div>
                     </div>
                 </div>
@@ -119,7 +121,7 @@
                         class="badge badge-info">6</span> </a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#"><i class="text-info ti-settings"></i>Settings</a>
-                <a class="dropdown-item" href="#"><i class="text-danger ti-unlock"></i>Logout</a>
+                <a class="dropdown-item" href="{{ route('logout') }}" ><i class="text-danger ti-unlock"></i>Logout</a>
             </div>
         </li>
     </ul>
