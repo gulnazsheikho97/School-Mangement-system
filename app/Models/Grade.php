d08f18a0ae9bb7d50eb6858e4c9b1de660f569a7
Kinda-halabi <kyndahlby@gmail.com>
2021-06-14 00:11:32 +0300

    add translatable to the model.

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+
 use Spatie\Translatable\HasTranslations;
+
 
 class Grade extends Model
 {
+    use HasTranslations;
+    protected $fillable=['name','notes'];
+    public $translatable = ['name'];//TRANSLATE ONLY NAME COLOMN
+    protected $table = 'Grade';
+    public $timestamps = true;
+
 
 
 }
